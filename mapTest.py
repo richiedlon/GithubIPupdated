6028f8cf5c177efdb211e704d1664f79ad5cf44c
Opegraphy <teddybearopeyemi@gmail.com>
2023-02-17 23:15:57 +0100

    updated Layout code

--- mapTest.py
+++ mapTest.py
@@ -2,40 +2,22 @@ import arcpy
 from arcpy.mp import *
 from arcpy.management import *
 
-aprx = arcpy.mp.ArcGISProject(r"C:\Users\xeon\Desktop\GIS IP Class\Python Tiede\mapping\project.aprx")
-map = aprx.listMaps()[0]
+aprx = arcpy.mp.ArcGISProject(r"C:\IPProject\IPProject_files\GithubIP\Vector\Spatial_Join\project.aprx")
+map = aprx.listMaps("First")[0]
 
-# changing Symbology
 # startvalue = ""
 # newlayer = map.addDataFromPath(r"C:\IPProject\IPProject_files\GithubIP\Vector\Spatial_Join/apartment" + startvalue + ".shp")
-newlayer = map.addDataFromPath(r"C:\IPProject\IPProject_files\GithubIP\Vector\Spatial_Join/apartmentfinalAreas1.shp")
-symbolLayer = (r"C:\Users\xeon\Desktop\IP Final Project\GithubIPupdated\Final Apartments.lyrx")
+newlayer = map.addDataFromPath(r"C:\IPProject\IPProject_files\GithubIP\Vector\Spatial_Join\apartmentfinalAreas2.shp")
+
+#Adding Symbology
+symbolLayer = (r"C:\IPProject\IPProject_files\GithubIP\Vector\Spatial_Join\Final Apartments.lyrx")
 symbologyField= ["VALUE_FIELD", "gridcode", "gridcode"],
 ApplySymbologyFromLayer(newlayer, symbolLayer, symbologyField)
 
-# creating the layout
-layout = aprx.listLayouts()[0]
-
+#Creating the layout
+layout = aprx.listLayouts()[1]
 outpath= r"C:\IPProject\IPProject_files\GithubIP\Vector\pdfs"
-layout.exportToPDF(outpath +"\ newmap.pdf", 300)
+layout.exportToPDF(outpath +"\ newmap.pdf")
 
-
-
-
-
-# map = aprx.listMaps()[0]
-# print(map.name)
-# layers = map.listLayers()[0]
-# print(layers.name)
-
-# mf = layout.listElements("MAPFRAME_ELEMENT")[0]
-# mfmap = mf.map
-# mfLayers = mfmap.listLayers()
-
-# for i in mfLayers:
-#     print(i.name)
-
-
-# for i in myLayers:
-#     print(myLayers.name)
-
+#Sanitation - remonve file no longer needed
+map.removeLayer(map.listLayers('apartmentfinalAreas2')[0])
